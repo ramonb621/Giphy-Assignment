4a301084b440f83833e647d4facd801db17eee8b
Ramon Barajas <ramonb621@yahoo.com>
2019-07-21 13:46:10 -0700

    got buttons to generate with new inputs and console log data/response appropriately

--- assets/app.js
+++ assets/app.js
@@ -1,37 +1,36 @@
-$(document).ready()
-
-function displayUpdatedSearches(){
-
-var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-vocation + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
+var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + vocations + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
 
 var vocations = ["cop", "nurse", "doctor", "plumber"];
+  
 
-  $.ajax({
+    $.ajax({
       url: queryURL,
       method: "GET"
-    }).then(function(data){
-
-    var newSearch = $("#gifs-display");
-    var rating = data.rating;
-    var pRating = $("<p>").text("Rating: " + rating);
-    newSearch.append(pRating);
-
-    var imageURL = data.url;
-    var image = $("<img>").attr("src", imageURL);
-    newSearch.append(image);
-
-    $("#gifs-display").prepend(newSearch)
     })
-    console.log(data);
+      .then(function(data){
+      
+        console.log(data);
+
+        var newSearchDiv = $("<div class='vocation'>");
+        var rating = data.rating;
+        var pRating = $("<p>").text("Rating: " + rating);
+        newSearchDiv.append(pRating);
+
+        var imageURL = data.url;
+        var image = $("<img>").attr("src", imageURL);
+        newSearchDiv.append(image);
+
+        $("#gifs-display").prepend(newSearchDiv);
+      })
   
 
   function renderButtons(){
+
       $("#buttons").empty();
 
       for(var i = 0; i < vocations.length; i++) {
           var a = $("<button>");
-          a.addId("vocation");
+          a.addClass("vocation-btn");
           a.attr("data-name", vocations[i]);
           a.text(vocations[i]);
           $("#buttons").append(a);
@@ -44,8 +43,9 @@ var vocations = ["cop", "nurse", "doctor", "plumber"];
         vocations.push(vocation);
         renderButtons();
       })
-      $(document).on("click", "#vocation", displayUpdatedSearches);
+      // $(document).on("click", ".vocation-btn", renderButtons);
       renderButtons();
+
 
   // STOP & START
     $(".gif").on("click", function() {
@@ -65,4 +65,3 @@ var vocations = ["cop", "nurse", "doctor", "plumber"];
     }
   
   });
-}
